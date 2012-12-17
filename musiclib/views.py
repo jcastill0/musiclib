03ecf0c6fe83343988fb36160579ebbc1be2a3c8
Julio Castillo <julio.castillo.2010@gmail.com>
2012-12-16 21:04:20 -0800

    weekly check-in

--- musiclib/views.py
+++ musiclib/views.py
@@ -49,28 +49,45 @@ def playlistAdd(request):
 
 
 def playlistSave(request):
+    buttonType = request.POST['plButton']
     playListName = request.POST['playlistName']
     playList = Playlist(name=playListName, owner=User.objects.get(pk=1))
     playList.save()
-    buttonType = request.POST['addButton']
     if (buttonType == 'saveNameOnly'):
 	return (playlistIndex(request))
     else:
-	return (playlistEdit(request, playList.id)) 
+	return (playlistEdit(request, playList.id))
+
 
 def playlistEdit(request, playlist_id):
     try:
 	playlist = Playlist.objects.get(pk=playlist_id)
     except Playlist.DoesNotExist:
 	raise Http404
-    songs = playlist.songs.all()
+    playListSongs = playlist.songs.all()
+    allSongs = Song.objects.all().order_by('-name')
     tmplt = loader.get_template('musiclib/playlist/editPlaylist.html')
     ctx = Context ({
 	'playlist': playlist,
-	'songs': songs,
+	'playListSongs': playListSongs,
+	'allSongs': allSongs,
 	})
     return HttpResponse(tmplt.render(ctx))
 
 
+def playlistSaveSongs(request):
+    playlist_id = request.POST['playlistID']
+    try:
+	playlist = Playlist.objects.get(pk=playlist_id)
+    except Playlist.DoesNotExist:
+	raise Http404
+    return (playlistIndex(request))
+
+
 def playlistDelete(request, playlist_id):
-    return HttpResponse("Delete")
+    try:
+        playList = Playlist.objects.get(pk=playlist_id)
+    except Playlist.DoesNotExist:
+        raise Http404
+    playList.delete()
+    return (playlistIndex(request))
